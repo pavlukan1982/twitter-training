93343d1ad52c4f60d440a725a57c6ffdd13f3f2b
pavluk <pavlukan1982@gmail.com>
2015-08-26 12:48:17 +0200

    Pax-exam

--- itest/src/test/java/by/pauliukevich/camel/BundleTest.java
+++ itest/src/test/java/by/pauliukevich/camel/BundleTest.java
@@ -8,7 +8,11 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
 
 import java.io.File;
 
+import javax.inject.Inject;
+
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.karaf.features.FeaturesService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
@@ -17,6 +21,9 @@ import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 
 @RunWith(PaxExam.class)
 public class BundleTest extends CamelTestSupport {
+
+	@Inject
+	protected FeaturesService featuresService;
 
 	@Configuration
 	public static Option[] configure() throws Exception {
@@ -30,9 +37,18 @@ public class BundleTest extends CamelTestSupport {
 						features(
 								maven().groupId("org.apache.camel.karaf").artifactId("apache-camel").type("xml")
 								.classifier("features").version("2.15.1.redhat-620133"), "camel-blueprint",
-						"camel-test"),
-								features(maven().groupId("by.pauliukevich").artifactId("feature").type("xml").classifier("features")
-						.version("1.0"), "blog-camel-blueprint-route"), keepRuntimeFolder() };
+						"camel-test"), keepRuntimeFolder() };
+		// ,
+		// features(maven().groupId("by.pauliukevich").artifactId("feature").type("xml").classifier("features")
+		// .version("1.0"), "camel-training-camel")
+	}
+
+	@Test
+	public void testProvisioning() throws Exception {
+
+		assertTrue(featuresService.isInstalled(featuresService.getFeature("camel-blueprint")));
+		// assertTrue(featuresService.isInstalled(featuresService.getFeature("camel-training-camel")));
+
 	}
 
 }
